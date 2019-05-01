f56d732d8801910ba2793594c9598ba26f9490f5
rockplayer007 <maildiroland@yahoo.it>
2019-05-01 14:04:43 +0200

    changed connection

--- service/DataLayer.js
+++ service/DataLayer.js
@@ -4,8 +4,8 @@ let { booksDbSetup } = require("./BookService");
 
 let sqlDb = sqlDbFactory({
   client: "pg",
-  //conncetion: "postgres://sblrblscxcwgzd:566feadcca87ac4674c9e0104276a3737e2cbab67fa2243e12cb4f54ae0956f9@ec2-54-246-92-116.eu-west-1.compute.amazonaws.com:5432/d82bvo13n4heii",
-  connection: "postgres://postgres:admin@localhost:5432/postgres",
+  conncetion: "postgres://sblrblscxcwgzd:566feadcca87ac4674c9e0104276a3737e2cbab67fa2243e12cb4f54ae0956f9@ec2-54-246-92-116.eu-west-1.compute.amazonaws.com:5432/d82bvo13n4heii?ssl=true",
+  //connection: "postgres://postgres:admin@localhost:5432/postgres",
   //connection: process.env.DATABASE_URL,
   ssl: true,
   debug: true
