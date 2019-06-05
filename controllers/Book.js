3310b9273fd1d954e2c8ba459b79984617f617a1
rockplayer007 <maildiroland@yahoo.it>
2019-06-05 23:44:59 +0200

    fixed yaml

--- controllers/Book.js
+++ controllers/Book.js
@@ -6,7 +6,6 @@ var Book = require('../service/BookService');
 module.exports.bookIdGET = function bookIdGET (req, res, next) {
   var id = req.swagger.params['id'].value;
 
-
     Book.bookIdGET(id)
     .then(function (response) {
       utils.writeJson(res, response);
